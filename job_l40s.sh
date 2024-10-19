0b6bc06649dd2a5d3025ea5b3516e5efd9670b59
muyildirim <55358436+muyildirim@users.noreply.github.com>
2024-10-19 03:10:27 +0200

    run 2.05 15 full epoch

--- job_l40s.sh
+++ job_l40s.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 8G
-#SBATCH --time 0:10:00
+#SBATCH --time 4:10:00
 #SBATCH --gpus 1
 #SBATCH --qos kuma
 #SBATCH --partition l40s
@@ -16,6 +16,6 @@ nvcc --version
 echo "======================================="
 python ./tool/check_gpu.py
 echo "======================================="
-python ./leo_wandb_sweep_2.py --machine_name kuma_L40S --small_toy --sweep_id hnlhb6il
+python ./leo_wandb_sweep_2.py --machine_name kuma_L40S --sweep_id hnlhb6il
 echo "==== Done ==================================="
 

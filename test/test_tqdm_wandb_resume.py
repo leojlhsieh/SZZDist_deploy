ffaa113d73bbd2e0194480d75b5fd3b73cfc2c06
muyildirim <55358436+muyildirim@users.noreply.github.com>
2024-10-24 16:46:27 +0200

    add gitignore

--- test/test_tqdm_wandb_resume.py
+++ test/test_tqdm_wandb_resume.py
@@ -18,11 +18,13 @@ print(args)
 os.environ["WANDB_API_KEY"] = 'c08ff557ed402c26a0c57ca0e7803529bdba9268'  # This is Leo's API key for the project
 wandb.login()
 n = 100_000
-with wandb.init(entity="leohsieh-epfl", project="szzbpm-distil-5", name='DDD') as run:
+with wandb.init(entity="leohsieh-epfl", project="szzbpm-distil-5", name='CCC') as run:
 # with wandb.init(entity="leohsieh-epfl", project="szzbpm-distil-5", id="", resume='allow') as run:
     wandb.log({"progress": 123})
+    print('wandb.log(progress: 123)')
     time.sleep(1)
     for i in tqdm(range(n), disable=not (args.verbose)):
+        # time.sleep(1)
         # time.sleep(1/n)
         wandb.log({"progress": i})
     print('Run finish')
@@ -31,14 +33,16 @@ print("With finished!")
 wandb.finish()
 print("wandb.finish() completed!")
 
-leohsieh-epfl/szzbpm-distil-5/cjqaq7ui
+# leohsieh-epfl/szzbpm-distil-5/cjqaq7ui
 
-wandb._sync()
+# wandb sync --id="cjqaq7ui" --entity="leohsieh-epfl" --project="szzbpm-distil-5"
 
 
---id	The run you want to upload to.
--p, --project	The project you want to upload to.
--e, --entity
+
+
+# --id	The run you want to upload to.
+# -p, --project	The project you want to upload to.
+# -e, --entity
 
 # %%
 exit()
